9063ec521e64b50a1edfc337084017076a920e90
MitchellGerdisch <mitch@pulumi.com>
2021-06-21 10:53:44 -0500

    actions test

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 
+// actions test 
+
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup");
 
