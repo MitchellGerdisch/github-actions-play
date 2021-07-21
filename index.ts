4041c17836108166e18ac444b2e6d2403a64a8fe
MitchellGerdisch <mitch@pulumi.com>
2021-07-21 11:47:21 -0500

    rebaseline

--- index.ts
+++ index.ts
@@ -6,19 +6,15 @@ import * as storage from "@pulumi/azure-native/storage";
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup");
+export const rgId = resourceGroup.id
 
 // Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
-});
+// const storageAccount = new storage.StorageAccount("sa", {
+//     resourceGroupName: resourceGroup.name,
+//     sku: {
+//         name: storage.SkuName.Standard_LRS,
+//     },
+//     kind: storage.Kind.StorageV2,
+// });
+// export const saId = storageAccount.id
 
-// Export the primary key of the Storage Account
-const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
-    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
-
-export const rgId = resourceGroup.id
