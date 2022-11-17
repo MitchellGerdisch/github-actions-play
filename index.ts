974264f1421856988dd4b10a9c24d9af7e079d65
MitchellGerdisch <mitch@pulumi.com>
2022-11-17 16:53:02 -0600

    random resources

--- index.ts
+++ index.ts
@@ -1,22 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources";
-import * as storage from "@pulumi/azure-native/storage";
+import * as random from "@pulumi/random";
 
-// actions test 
+const config = new pulumi.Config();
+const petNameLength = config.getNumber("petNameLength") ?? 5
 
-// Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
+const randomPet = new random.RandomPet("actionsPet", {
+    length: petNameLength
+})
 
-// Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
-    },
-    kind: storage.Kind.StorageV2,
-});
-
-// Export the primary key of the Storage Account
-const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
-    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
