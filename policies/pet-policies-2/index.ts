e715e5d8666c04c5b4f1179f3d1081974a71ad1c
MitchellGerdisch <mitch@pulumi.com>
2023-01-12 13:29:50 -0600

    test with multiple policy packs

--- policies/pet-policies-2/index.ts
+++ policies/pet-policies-2/index.ts
@@ -4,10 +4,10 @@ import { PolicyPack, validateResourceOfType } from "@pulumi/policy";
 new PolicyPack("random-pet-name", {
     policies: [{
         name: "pet-name-length",
-        description: "Prohibits short pet-name-lengths.",
+        description: "Prohibits short pet-name-lengths (part 2)",
         enforcementLevel: "advisory",
         validateResource: validateResourceOfType(random.RandomPet, (pet, args, reportViolation) => {
-            const requiredPetLength = 4
+            const requiredPetLength = 8
             if ((pet.length) && (pet.length < requiredPetLength)) {
                 reportViolation(
                     `pet name should be at least ${requiredPetLength} characters long`
